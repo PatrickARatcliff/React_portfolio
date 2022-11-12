11cf2888939064cb701fc725379a905a761c6750
PatrickARatcliff <pratcliff5@gmail.com>
2022-11-12 16:57:24 -0700

    crteate file structure, populate src files, mock public files, create package.json

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ReactPort from './components/ReactPort';
+
+function App() {
+  return (
+    <div className="react-app">
+      <ReactPort />
+    </div>
+  );
+}
+
+export default App;
