8c09c47cc85f2f762981dfa95bfadfadb576a026
PatrickARatcliff <pratcliff5@gmail.com>
2022-11-12 18:31:22 -0700

    create jsx return for ContactForm.js

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -20,11 +20,102 @@ function ContactForm(props) {
 
     return !props.edit ? (
         <div>
+            {/* <!--Section: Contact v.2--> */}
+            <section class="mb-4">
+                {/* <!--Section heading--> */}
+                <h2 class="h1-responsive font-weight-bold text-center my-4">Contact Me</h2>
+                {/* <!--Section description--> */}
+                <p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact me directly.</p>
+                <div class="row">
+                    {/* <!--Grid column--> */}
+                    <div class="col-md-9 mb-md-0 mb-5">
+                        <form id="contact-form" name="contact-form" action="mail.php" method="POST">
+                            {/* <!--Grid row--> */}
+                            <div class="row">
+                                {/* <!--Grid column--> */}
+                                <div class="col-md-6">
+                                    <div class="md-form mb-0">
+                                        <input
+                                            type="text"
+                                            id="name"
+                                            name="name"
+                                            className="form-control"
+                                        ></input>
+                                        <label for="name" className="">Your name</label>
+                                    </div>
+                                </div>
+                                {/* <!--Grid column--> */}
+                                {/* <!--Grid column--> */}
+                                <div class="col-md-6">
+                                    <div class="md-form mb-0">
+                                        <input
+                                            type="text"
+                                            id="email"
+                                            name="email"
+                                            className="form-control"
+                                        ></input>
+                                        <label for="email" class="">Your email</label>
+                                    </div>
+                                </div>
+                                {/* <!--Grid column--> */}
+                            </div>
+                            {/* <!--Grid row--> */}
+                            {/* <!--Grid row--> */}
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="md-form mb-0">
+                                        <input
+                                            type="text"
+                                            id="subject"
+                                            name="subject"
+                                            className="form-control"
+                                        ></input>
+                                        <label for="subject" class="">Subject</label>
+                                    </div>
+                                </div>
+                            </div>
+                            {/* <!--Grid row--> */}
+                            {/* <!--Grid row--> */}
+                            <div class="row">
+                                {/* <!--Grid column--> */}
+                                <div class="col-md-12">
 
+                                    <div class="md-form">
+                                        <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
+                                        <label for="message">Your message</label>
+                                    </div>
+                                </div>
+                            </div>
+                            {/* <!--Grid row--> */}
+                        </form>
+                        <div class="text-center text-md-left">
+                            <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
+                        </div>
+                        <div class="status"></div>
+                    </div>
+                    {/* <!--Grid column--> */}
+                    {/* <!--Grid column--> */}
+                    <div class="col-md-3 text-center">
+                        <ul class="list-unstyled mb-0">
+                            <li><i class="fas fa-map-marker-alt fa-2x"></i>
+                                <p>Aurora, CO 80012, USA</p>
+                            </li>
+                            <li><i class="fas fa-phone mt-4 fa-2x"></i>
+                                <p>303.579.5842</p>
+                            </li>
+                            <li><i class="fas fa-envelope mt-4 fa-2x"></i>
+                                <p>pratcliff5@gmail.com</p>
+                            </li>
+                        </ul>
+                    </div>
+                    {/* <!--Grid column--> */}
+                </div>
+            </section>
+            {/* <!--Section: Contact v.2--> */}
         </div>
     ) : (
         <div>
-
+            <h3>THANK YOU!</h3>
         </div>
     );
 }
